9ad85cf7ab7daa4fa6415f66fc7aa6dbf2585139
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-11-01 17:31:11 -0300

    Exercício 1 finalizado

--- 01_BotasTrocadas/main.cpp
+++ 01_BotasTrocadas/main.cpp
@@ -15,26 +15,33 @@ void print(vector<Sapato> sap, int nEl){
     for(int i = 0; i < nEl; i++) {
         cout << sap[i].tamanho << sap[i].lado << endl;
     }
-    cout << "--------------------------------" << endl;
+    cout << "---------------" << nEl << "-----------------" << endl;
 }
 
-void solve(vector<Sapato> sap, int nEl){
+void solve(vector<Sapato> sap, int nEl, int * res){
     int elements = nEl;
     for(int i = 0; i < nEl; i++){
         if((sap[0].tamanho == sap[i].tamanho) && (sap[0].lado != sap[i].lado)){
             sap.erase(sap.begin() + i);
             sap.erase(sap.begin());
+            *res = *res + 1;    
             elements-=2;
-            print(sap, nEl-2);
-            if(elements >= 2) solve(sap, nEl - 2);
+            if(elements >= 2) {
+                //print(sap, elements);
+                solve(sap, elements, res);
+            }
+            break;
         }
     }
 }
 
 int main(){
-    int nSapatos, tam, *res = 0;
+    int nSapatos, tam, resposta = 0;
+    int *res;
+    res = &resposta;
     char pe;
     vector<Sapato> shoe;
+    
     cin >> nSapatos;
 
     for(int i = 0; i < nSapatos; i++) {
@@ -43,9 +50,9 @@ int main(){
         shoe.push_back(Sapato{tam, pe});
     }
 
-    solve(shoe, nSapatos);
+    solve(shoe, nSapatos, res);
 
-    cout << res << endl;
+    cout << resposta << endl;
 
     return 0;
 }
